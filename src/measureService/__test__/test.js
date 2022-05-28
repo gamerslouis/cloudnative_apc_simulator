b47bc8f45a911eca1a03906150831fe81129b368
alan890104 <alan890104@gmail.com>
2022-05-28 15:10:41 +0800

    feat: remote mock test.js

--- src/measureService/__test__/test.js
+++
@@ -1,9 +0,0 @@
-describe("Calculator tests", () => {
-  test("adding 1 + 2 should return 3", () => {
-    // arrange and act
-    var result = 3;
-
-    // assert
-    expect(result).toBe(3);
-  });
-});
