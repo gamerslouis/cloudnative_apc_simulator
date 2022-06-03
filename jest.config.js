f9fc6367a4362b2f7ccf932d611e473436c92da2
gamerslouis <gamerslouis@gmail.com>
2022-06-04 05:36:43 +0800

    Fix jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   coverageDirectory: 'coverage',
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageReporters: [
+    'clover',
+    'json',
+    'lcov',
+    'text',
+    'text-summary',
+    'json-summary',
+  ],
 };
